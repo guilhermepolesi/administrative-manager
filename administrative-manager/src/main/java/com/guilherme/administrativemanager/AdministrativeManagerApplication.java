5d2e06ff277ae05b8bce7c6de0b94f66b1b4f07e
guilhermepolesi <guilherme-polesi@hotmail.com>
2024-02-05 18:02:29 -0300

    Configuring the  spring project

---
+++ administrative-manager/src/main/java/com/guilherme/administrativemanager/AdministrativeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.administrativemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministrativeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministrativeManagerApplication.class, args);
+	}
+
+}
